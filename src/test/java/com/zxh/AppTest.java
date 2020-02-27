49a237f1fb7bc9c1877fa786d9d6f0c5fc409845
zxh <2439982132@qq.com>
2020-02-27 21:31:47 +0800

    Initial commit

---
+++ src/test/java/com/zxh/AppTest.java
@@ -0,0 +1,23 @@
+package com.zxh;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        SqlSession session = null;
+
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
